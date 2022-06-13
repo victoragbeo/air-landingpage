6869f56a0106431bb74ec0e19fe12abd7ac841cf
Victor Agbeo <75548415+saintvic@users.noreply.github.com>
2022-06-12 22:42:20 -0500

    Card Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero(){
+    return(
+        <section className="hero">
+            <img src="./images/photo-grid.png" alt="" className="hero--photo" />
+            <h1 className="hero--">Online Experiences</h1>
+            <p>Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
+
+export default Hero;
