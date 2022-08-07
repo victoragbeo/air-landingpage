edbd66e7954e14480e4773df1cea53b8073953b8
Victor Agbeo <75548415+victoragbeo@users.noreply.github.com>
2022-08-07 17:49:50 -0500

    "root"

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const data = [
     {
         id:1,
-        title:"Life Lessons with Victor Agbeo",
+        title:"Life Lessons with Victor",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price:600,
         coverImg:"./images/lake.png",
@@ -15,7 +15,7 @@ const data = [
     },
     {
         id: 2,
-        title: "Learn Wedding Photography",
+        title: "Learn Wedding Photography with Rachael",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 300,
         coverImg:"./images/rachael.png" ,
