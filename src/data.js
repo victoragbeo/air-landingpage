d17dce6bfa17127f62b392fe89bdc58c441439c3
Victor Agbeo <75548415+victoragbeo@users.noreply.github.com>
2022-08-25 19:23:18 -0500

    'k'

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const data = [
         title:"Life Lessons with Victor",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price:600,
-        coverImg:"./images/lake.png",
+        coverImg:"./images/shoes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
